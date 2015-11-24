05ea821398e361a91ec0c546210838ac29eda9a5
skratchdot <git@skratchdot.com>
2015-11-24 12:05:58 -0500

    adding github-corners

--- app/js/Page.js
+++ app/js/Page.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from 'react-bootstrap';
 import Header from './Header';
 import Footer from './Footer';
+import GithubCorner from './GithubCorner';
 
 module.exports = React.createClass({
 	render: function () {
@@ -11,6 +12,7 @@ module.exports = React.createClass({
 					<Header active={this.props.pageName} />
 					{this.props.children}
 					<Footer />
+					<GithubCorner href="https://github.com/skratchdot/colorify" />
 				</Grid>
 			</div>
 		);
